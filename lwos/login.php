d0ed47785b7ebd06cf834da2befa16eadbbbc4d0
Muhammad Abdul Qaium <qaiums@gmail.com>
2016-11-28 08:21:24 +0600

    This is my webtech project. I am going to make a forum. Pull-1

---
+++ lwos/login.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html PUBLIC>
+<html xmlns="">
+<head>
+<meta/>
+<title>Learn with online society</title>
+ <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="header_panel">
+	<div id="header_section">
+    	<div id="title_section">Learn With Online Society</div>
+      <div id="tagline">about my website</div>
+    </div>
+</div>
+<div id="menu_panel">
+    <div id="menu_section">
+        <ul>
+            <li><a href="index.php"  class="current">Home</a></li>
+            <li><a href="" >Profile</a></li>
+            <li><a href="" >Follower</a></li>            
+            <li><a href="" >Following</a></li>  
+            <li><a href="" >About Us</a></li> 
+            <li><a href="" >Contact Us</a></li>
+            <li><a href="login.php" >Login</a></li>  
+            <li><a href="" >Register</a></li>                       
+        </ul> 
+    </div>
+</div>
+<!-- colum 1-->
+<div id="content">
+
+	<div id="content_column_one">
+    	<div class="column_one_section">
+        	<h1>Categories</h1>
+            <ul>
+                <li><a href="#">Oracle </a></li>
+                <li><a href="#">PHP</a></li>
+                <li><a href="#">Java</a></li>
+                <li><a href="#">C#</a></li>
+                <li><a href="#">C++</a></li>
+                
+            </ul>                  
+        </div>
+        
+        <div class="cleaner_with_divider">&nbsp;</div>
+        
+        
+     
+    </div>
+
+    <!-- end of column one -->
+    
+   	<div id="content_column_two">
+    
+    	<div class="column_two_section">
+			<center>
+  <form action="check.php" method="post">
+  <br>
+<br>
+<pre>
+    <h1>Give your password and username to login</h1>
+<p>User Name : <input value="" type="text" name="uname" /><br><br>
+Password  : <input type="password" name="pass" /><br><br></p> 
+      
+      </pre>
+      <input type="submit" value="Login" /><br><br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+   
+  </form>
+</center>
+               
+        </div>
+        
+    </div> <!-- end of column two -->
+
+    <div id="content_column_three">
+    	
+        
+    	<div class="column_three_section">
+            <h1>Popular Posts</h1>
+            
+        </div>
+               
+        <div class="cleaner_with_divider">&nbsp;</div>
+        
+        <div class="column_three_section">
+            <h1>About This Blog</h1>
+            <p>Etiam fringilla vestibulum nulla. Suspendisse venenatis. Cum sociis natoque penatibus et magnis  montes, nascetur ridiculus mus. Morbi dapibus dolor sit amet metus suscipit iaculis. <a href="#">read more</a></p>
+      </div>  
+          
+    </div> <!-- end of column three -->   
+    
+    <div class="cleaner">&nbsp;</div>
+</div> <!-- end of content -->
+
+<div id="bottom_panel">
+ <center>
+	<div class="bottom_panel_section">
+   	<a href="#">Home</a> | <a href="#">Profile</a> | <a href="#"> Follower</a> | <a href="#">Following </a>| <a href="#">About Us</a> | <a href="#">Contact Us</a><br /><br />
+  <p> Copyright © 2016 </p> <a href="#"><strong>Muhammad Abdul Qaium</strong></a></div>
+    </center>
+    
+
+    <div class="cleaner">&nbsp;</div>
+</div> <!-- end of bottom panel -->
+
+
+
+<!-- Java script-->
+
+<script type="text/javascript">
+	function login()
+	{
+		document.getElementById("posts").innerHTML=
+	}
+</script>
+
+
+</body>
+</html>
